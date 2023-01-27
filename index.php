c89339d0013bdde518b3cedc14016da2ac379a28
Sergio Ospina <sergio.ospinad@gmail.com>
2023-01-26 22:49:22 -0500

    Updating index with URL customization and creating home file

---
+++ index.php
@@ -0,0 +1,46 @@
+
+<!-- Customization of URL's and protection of folders and files   -->
+<?php
+    $route = !empty($_GET['url']) ? $_GET['url'] : "Home/index";
+
+    $arrayIndex = explode("/", $route);
+    $controller = $arrayIndex[0];
+    $method = "index";
+    $param = "";
+
+    //Verify if $method exists
+
+    if (!empty($arrayIndex[1])) {
+        
+        if(!empty($arrayIndex[1] != "")){
+            $method = $arrayIndex[1];
+        }
+    }
+    if (!empty($arrayIndex[2])) {
+        
+        if(!empty($arrayIndex[2] != "")){
+            for ($i=2; $i < count($arrayIndex) ; $i++) { 
+                $param .= $arrayIndex[$i]. ",";
+            }
+            $param = trim($param, ",");
+        }
+    }
+
+    //Store the folders route of Controller directory
+    $dirControllers = "Controllers/".$controller.".php";
+    if (file_exists($dirControllers)) {
+        require_once $dirControllers;
+        $controller = new $controller();
+        if (method_exists($controller, $method)) {
+            $controller->$method($param);
+        }else {
+            echo "The method doesn't exists";
+        }
+    }else {
+        echo "The controller doesn't exists";
+    }
+    
+
+
+
+?>
